300fc60e0a320bceb1a65a90c8acb7941609185b
Clement Zotti <clement.zotti@imeka.ca>
2019-12-22 17:30:13 -0500

    Fix most pylint error/warn.

--- pynbp/__init__.py
+++ pynbp/__init__.py
@@ -1,5 +1,12 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""
+Python Numeric Broadcast Protocol
+
+This module implements HP Tuners / Track Addict Numeric Broadcast Protocol
+
+WiFI Implementation
+"""
+
 import time
 from collections import namedtuple
 import logging
@@ -9,29 +16,27 @@ import socket
 
 import serial
 
-"""
-Python Numeric Broadcast Protocol
+__version__ = "0.0.9"
+HOME = str(Path.home())
 
-This module implements HP Tuners / Track Addict Numeric Broadcast Protocol
+NbpKPI = namedtuple("NbpKPI", "name, unit, value")
+NbpPayload = namedtuple("NbpPayload", "timestamp, packettype, nbpkpilist")
 
-WiFI Implementation
-"""
+LOGGER = logging.getLogger("pynbp")
+FH = logging.FileHandler("{0}/pynbp.log".format(HOME))
+FH.setLevel(logging.INFO)
+FH.setFormatter(logging.Formatter(
+    "%(asctime)s - %(name)s - %(levelname)s\n%(message)s"))
+LOGGER.addHandler(FH)
 
-__version__='0.0.9'
-home = str(Path.home())
-
-NbpKPI = namedtuple('NbpKPI', 'name, unit, value')
-NbpPayload = namedtuple('NbpPayload', 'timestamp, packettype, nbpkpilist')
-
-logger = logging.getLogger('pynbp')
-fh = logging.FileHandler('{0}/pynbp.log'.format(home))
-fh.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s\n%(message)s')
-fh.setFormatter(formatter)
-logger.addHandler(fh)
 
 class BasePyNBP(threading.Thread):
-    def __init__(self, nbpqueue, device_name='PyNBP', protocol_version='NBP1', min_update_interval=0.2):
+    """
+    Base class taht define package creation for Wifi and Bluetooth
+    """
+
+    def __init__(self, nbpqueue, device_name="PyNBP",
+                 protocol_version="NBP1", min_update_interval=0.2):
         self.device_name = device_name
         self.protocol_version = protocol_version
         self.last_update_time = 0
@@ -42,41 +47,58 @@ class BasePyNBP(threading.Thread):
         self.min_update_interval = min_update_interval
         threading.Thread.__init__(self)
 
+    def run(self):
+        raise NotImplementedError
 
-    def run():
-        raise NotImplemented
-
-    def metedata(self):
+    def metadata(self):
+        """
+        Get metadata for the device, device_name
+        """
         return str.encode("@NAME:{0}\n\n".format(self.device_name))
 
-    def _genpacket(self, type='ALL'):
-        packet="*{0},{1},{2:.6f}\n".format(self.protocol_version, type, self.packettime)
+    def _genpacket(self, ptype='ALL'):
+        if ptype not in ["ALL", "UPDATE", "METADATA"]:
+            raise ValueError("Unrecognize ptype: {}".format(ptype))
+        if ptype == "METADATA":
+            return self.metadata()
 
-        if self.updatelist and type != 'ALL':
+        packet = "*{0},{1},{2:.6f}\n".format(
+            self.protocol_version, ptype, self.packettime)
+
+        if self.updatelist and ptype != "ALL":
             kpis = [self.kpis[k] for k in self.updatelist]
         else:
             kpis = self.kpis.values()
 
-
         for kpi in kpis:
             if kpi.unit:
-                packet+='"{0}","{1}":{2}\n'.format(kpi.name, kpi.unit, kpi.value)
+                packet += '"{0}","{1}":{2}\n'.format(
+                    kpi.name, kpi.unit, kpi.value)
             else:
-                packet+='"{0}":{1}\n'.format(kpi.name, kpi.value)
+                packet += '"{0}":{1}\n'.format(kpi.name, kpi.value)
 
-        packet+="#\n\n"
+        packet += "#\n\n"
 
         return str.encode(packet)
 
 
 class PyNBP(BasePyNBP):
-    def __init__(self, nbpqueue, device='/dev/rfcomm0', device_name='PyNBP', protocol_version='NBP1', min_update_interval=0.2):
-        super().__init__(nbpqueue, device_name=device_name, protocol_version=protocol_version, min_update_interval=min_update_interval)
+    """
+    Defines Bluetooth communication of the NBP protocol.
+    """
+
+    def __init__(self, nbpqueue, device="/dev/rfcomm0", device_name="PyNBP",
+                 protocol_version="NBP1", min_update_interval=0.2):
+        super().__init__(
+            nbpqueue,
+            device_name=device_name,
+            protocol_version=protocol_version,
+            min_update_interval=min_update_interval)
         self.device = device
 
     def run(self):
-        connected=False
-        serport=None
+        connected = False
+        serport = None
 
         while True:
             nbppayload = self.nbpqueue.get()
@@ -90,52 +112,68 @@ class PyNBP(BasePyNBP):
 
             if not connected:
                 try:
-                    serport=serial.serial_for_url(self.device)
-                    connected=True
-                except:
-                    logging.info('Comm Port conection not open - waiting for connection')
+                    serport = serial.serial_for_url(self.device)
+                    connected = True
+                except serial.SerialException as _:
+                    logging.info(
+                        "Comm Port conection not open - waiting for connection")
 
-            if connected and serport.is_open:
-                try:
-                    if serport.in_waiting > 0:
-                        logger.info(serport.read(serport.in_waiting).decode())
+            if not connected and not serport.is_open:
+                continue
+            try:
+                if serport.in_waiting > 0:
+                    LOGGER.info(serport.read(serport.in_waiting).decode())
 
-                    if time.time() - self.last_update_time > self.min_update_interval:
-                        if nbppayload.packettype == 'UPDATE':
-                            nbppacket=self._genpacket(type=nbppayload.packettype)
-                        elif nbppayload.packettype == 'ALL':
-                            nbppacket=self._genpacket(type=nbppayload.packettype)
-                        elif nbppayload.packettype == 'METADATA':
-                            nbppacket=self.metedata()
-                        else:
-                            logging.info('Invalid packet type {0}.'.format(packettype))
+                if time.time() - self.last_update_time < self.min_update_interval:
+                    continue
+                # if nbppayload.packettype == 'UPDATE':
+                #     nbppacket = self._genpacket(
+                #         ptype=nbppayload.packettype)
+                # elif nbppayload.packettype == 'ALL':
+                #     nbppacket = self._genpacket(
+                #         ptype=nbppayload.packettype)
+                # elif nbppayload.packettype == 'METADATA':
+                #     nbppacket = self.metadata()
+                # else:
+                #     logging.info(
+                #         'Invalid packet type {}.'.format(
+                #             nbppayload.packettype))
+                nbppacket = self._genpacket(nbppayload.packettype)
 
-                        logger.warning(nbppacket.decode())
+                LOGGER.warning(nbppacket.decode())
 
-                        serport.write(nbppacket)
-                        self.updatelist = []
-                        self.last_update_time = time.time()
+                serport.write(nbppacket)
+                self.updatelist = []
+                self.last_update_time = time.time()
 
-                except:
-                    logging.exception('Serial Write Failed. Closing port.')
-                    serport.close()
-                    connected = False
+            except serial.SerialTimeoutException as _:
+                logging.exception("Serial Write timeout. Closing port.")
+                serport.close()
+                connected = False
+
 
 class WifiPyNBP(BasePyNBP):
-    def __init__(self, nbpqueue, ip='127.0.0.1', port=35000, device_name='PyNBP', protocol_version='NBP1', min_update_interval=0.2):
-        super().__init__(nbpqueue, device_name=device_name, protocol_version=protocol_version, min_update_interval=min_update_interval)
-        self.ip = ip
-        self.port = port
+    """
+    Defines the Wifi communication of NBP protocol.
+    """
+
+    def __init__(self, nbpqueue, ip="127.0.0.1", port=35000, device_name="PyNBP",
+                 protocol_version="NBP1", min_update_interval=0.2):
+        super().__init__(
+            nbpqueue,
+            device_name=device_name,
+            protocol_version=protocol_version,
+            min_update_interval=min_update_interval)
+        self._ip = ip
+        self._port = port
 
     def run(self):
-        connected=False
-        serport=None
+        connected = False
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.settimeout(1.0)
-        logging.warning('Binding to {0}:{1}'.format(self.ip, self.port))
-        sock.bind((self.ip,self.port))
+        logging.warning("Binding to {}:{}".format(self._ip, self._port))
+        sock.bind((self._ip, self._port))
         sock.listen(1)
-
 
         while True:
             nbppayload = self.nbpqueue.get()
@@ -150,39 +188,48 @@ class WifiPyNBP(BasePyNBP):
             if not connected:
                 try:
                     conn, client_address = sock.accept()
-                    connected=True
-                    logging.warning('Connection from {0} open'.fomat(client_address))
-                except:
-                    logging.info('Socket conection not open - waiting for connection')
+                    connected = True
+                    logging.warning(
+                        "Connection from {0} open".format(client_address))
+                except InterruptedError as _:
+                    logging.info(
+                        "Socket conection not open - waiting for connection")
 
-            if connected:
-                try:
-                    data = conn.recv(1024)
-                    if data:
-                        text=data.decode().strip()
-                        logger.info(text)
-                        if text == "!ALL":
-                            logging.warning('ALL Packet Requested. Sending')
-                            conn.sendall(self._genpacket('ALL'))
+            if not connected:
+                continue
+            try:
+                data = conn.recv(1024)
+                if data:
+                    text = data.decode().strip()
+                    LOGGER.info(text)
+                    if text == "!ALL":
+                        logging.warning("ALL Packet Requested. Sending")
+                        conn.sendall(self._genpacket('ALL'))
 
-                    if time.time() - self.last_update_time > self.min_update_interval:
-                        if nbppayload.packettype == 'UPDATE':
-                            nbppacket=self._genpacket(type=nbppayload.packettype)
-                        elif nbppayload.packettype == 'ALL':
-                            nbppacket=self._genpacket(type=nbppayload.packettype)
-                        elif nbppayload.packettype == 'METADATA':
-                            nbppacket=self.metedata()
-                        else:
-                            logging.info('Invalid packet type {0}.'.format(packettype))
+                if time.time() - self.last_update_time < self.min_update_interval:
+                    continue
 
-                        logger.warning(nbppacket.decode())
+                # if nbppayload.packettype == 'UPDATE':
+                #     nbppacket = self._genpacket(
+                #         ptype=nbppayload.packettype)
+                # elif nbppayload.packettype == 'ALL':
+                #     nbppacket = self._genpacket(
+                #         ptype=nbppayload.packettype)
+                # elif nbppayload.packettype == 'METADATA':
+                #     nbppacket = self.metadata()
+                # else:
+                #     logging.info(
+                #         'Invalid packet type {0}.'.format(
+                #             nbppayload.packettype))
+                nbppacket = self._genpacket(nbppayload.packettype)
 
-                        conn.sendall(nbppacket)
-                        self.updatelist = []
-                        self.last_update_time = time.time()
+                LOGGER.warning(nbppacket.decode())
 
-                except:
-                    logging.exception('Wifi Write Failed. Closing port.')
-                    conn.close()
-                    connected = False
+                conn.sendall(nbppacket)
+                self.updatelist = []
+                self.last_update_time = time.time()
 
+            except Exception as _:  # TODO: Find the good one.
+                logging.exception("Wifi Write Failed. Closing port.")
+                conn.close()
+                connected = False
